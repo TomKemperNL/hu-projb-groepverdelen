95637858238e162b03147b016da2883d4143b3ea
Tom <tom.kemper@hu.nl>
2021-11-13 16:11:41 +0100

    scrolling is hard!

--- groepverdelen/gui/__init__.py
+++ groepverdelen/gui/__init__.py
@@ -10,7 +10,19 @@ def create_gui(messages, command_handlers, process_message, model):
     root = tkinter.Tk()
     columns = 3
 
-    teams_frame = tkinter.Frame(root)
+    main_frame = tkinter.Frame(root)
+    main_frame.pack()
+    canvas = tkinter.Canvas(main_frame)
+    scrollbar = tkinter.Scrollbar(canvas, orient=tkinter.VERTICAL, command=canvas.yview)
+    content_frame = tkinter.Frame(canvas)
+    content_frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
+                       )
+    canvas.create_window((0, 0), window=content_frame, anchor='nw')
+    canvas.configure(yscrollcommand=scrollbar.set)
+    content_frame.pack()
+    canvas.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True)
+    scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+    teams_frame = tkinter.Frame(content_frame)
     teams_frame.columnconfigure(tuple(range(0, columns)), weight=1)
 
     index = 0
@@ -21,7 +33,7 @@ def create_gui(messages, command_handlers, process_message, model):
 
     teams_frame.pack()
 
-    controls_frame = tkinter.Frame(root)
+    controls_frame = tkinter.Frame(content_frame)
 
     selected = tkinter.StringVar(controls_frame)
     team_names = list(model.keys())
@@ -35,7 +47,7 @@ def create_gui(messages, command_handlers, process_message, model):
     for c in [team_label, dropdown, name_label, name_field]:
         c.pack(side=tkinter.LEFT)
 
-    slider_frame = tkinter.Frame(root)
+    slider_frame = tkinter.Frame(content_frame)
     for program in range(0, len(groups.PROGRAM_NAMES)):
         scale = tkinter.Scale(slider_frame, from_=1, to=len(groups.VALUES), orient=tkinter.HORIZONTAL, showvalue=False)
         scale.pack()
