19e7a8b3d81260782db79ccc8994473f6a354858
Tom <tom.kemper@hu.nl>
2021-11-20 11:24:55 +0100

    Replace Bottle with Flask

--- groepverdelen/web.py
+++ groepverdelen/web.py
@@ -1,4 +1,4 @@
-from bottle import get, post, static_file, run, request, template
+from flask import Flask, render_template
 from messages import ADD_STUDENT, REMOVE_STUDENT
 
 # If you're thinking 'what weird Python is this??', it's demo-code and I'm not allowed to use classes yet:)
@@ -6,32 +6,35 @@ import groups
 
 
 def create_web(messages, handlers):
-    @get('/')
+
+    app = Flask(__name__, template_folder='views')
+
+    @app.get('/')
     def index():
-        return static_file('index.html', 'groepverdelen/views')
+        return render_template('index.html')
 
-    @post('/join')
-    def join_group():
-        name = request.forms.get('name')
-        team = request.forms.get('team')
-        choices = []
-        for program in groups.PROGRAM_NAMES:
-            value = request.forms.get(program)
-            if value is not None:
-                choices.append((program, int(value)))
-
-        if groups.is_valid_groupchoice(choices):
-            messages.append((ADD_STUDENT, (team, name, choices)))
-            return template("Ok {{name}}", name=name)
-        else:
-            return template("Invalid choices", name=name, status=400)
-
-    @post('/leave')
-    def join_group():
-        name = request.forms.get('name')
-        messages.append((REMOVE_STUDENT, name))
-        return template("Doei {{name}}", name=name)
+    # @app.post('/join')
+    # def join_group():
+    #     name = request.forms.get('name')
+    #     team = request.forms.get('team')
+    #     choices = []
+    #     for program in groups.PROGRAM_NAMES:
+    #         value = request.forms.get(program)
+    #         if value is not None:
+    #             choices.append((program, int(value)))
+    #
+    #     if groups.is_valid_groupchoice(choices):
+    #         messages.append((ADD_STUDENT, (team, name, choices)))
+    #         return template("Ok {{name}}", name=name)
+    #     else:
+    #         return template("Invalid choices", name=name, status=400)
+    #
+    # @app.post('/leave')
+    # def join_group():
+    #     name = request.forms.get('name')
+    #     messages.append((REMOVE_STUDENT, name))
+    #     return template("Doei {{name}}", name=name)
 
     return {
-        'start': lambda: run(host='0.0.0.0', port=80)
+        'start': lambda: app.run(host='0.0.0.0', port=80)
     }
